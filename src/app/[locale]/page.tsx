721f7872cc24f9e282aaff8eb8bfa97b3c21ced6
Khalil El Ghoul <khalilelghoul01@gmail.com>
2024-07-11 10:43:21 +0200

    update

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -20,7 +20,9 @@ function page() {
   const tr = useTranslator();
   return (
     <>
-      <BgSVG />
+      {/* <BgSVG />
+       */}
+      <div className="absolute inset-0 -z-10 h-full w-full items-center px-5 py-24 [background:radial-gradient(125%_125%_at_50%_10%,#000_40%,#63e_100%)]"></div>
       <Navbar />
       {/* 
       2 screen h div
